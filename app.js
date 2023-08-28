1cf784f124372b7ffffdf5682ed787d575c58159
Kevin Christ <kevin.christ@stud.uni-heidelberg.de>
2023-08-28 10:47:38 +0200

    Vue added the vue instance and some data

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            username: "",
+            password: "",
+        };
+    },
+    methods:{
+        
+    },
+});
+
+app.mount("#app");
